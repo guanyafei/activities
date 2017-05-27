3fc9c262ed63cfe8bc51e5501570ace21f277c7f
guanyafei <1902749059@qq.com>
2017-05-27 16:32:40 +0800

    完成

--- SAP/dist/js/index.js
+++ SAP/dist/js/index.js
@@ -1,1 +1,33 @@
-"use strict";
+'use strict';
+
+//轮播图
+var mySwiper = new Swiper('.swiper-container01', {
+    autoplay: 5000, //可选选项，自动滑动
+    speed: 300,
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    prevButton: '.swiper-button-prev',
+    nextButton: '.swiper-button-next',
+    spaceBetween: 20
+});
+//轮播图
+var mySwiper = new Swiper('.swiper-container02', {
+    autoplay: 5000, //可选选项，自动滑动
+    speed: 400,
+    slidesPerView: 2,
+    slidesPerGroup: 2,
+    prevButton: '.swiper-button-prev',
+    nextButton: '.swiper-button-next',
+    spaceBetween: 20
+});
+
+//轮播图
+var mySwiper = new Swiper('.swiper-container03', {
+    autoplay: 5000, //可选选项，自动滑动
+    speed: 600,
+    slidesPerView: 2,
+    slidesPerGroup: 2,
+    prevButton: '.swiper-button-prev',
+    nextButton: '.swiper-button-next',
+    spaceBetween: 20
+});
